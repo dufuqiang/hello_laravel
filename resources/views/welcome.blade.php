6ecaf48653d3271b9fd77f2ba7201d1d4ed2f19d
dufuqiang <302952688@qq.com>
2017-05-04 12:58:20 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 By DZ</div>
+                <div class="title">Laravel 5 By dufuqiang</div>
             </div>
         </div>
     </body>
